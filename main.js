b0e89c55b590016eb54779a07f3d623311efed46
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-18 08:00:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 var canvas = document.getElementById("screen");
 var context = canvas.getContext("2d");
 
+function main() {
+   context.fillStyle = "red";
+   context.fillRect(20, 20 , 50, 50);
+}
 
-context.fillStytle="red";
-context.fillRect(20, 20, 50, 50);
-
-if(event.keyCode == 37) {
-   alert('Left was pressed');
-   }else if(event.keyCode == 39)
-   {
-   alert('Right was pressed');
-   }
-
+setInterval(main,  1000 / 60);
