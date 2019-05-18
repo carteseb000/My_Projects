6c806775958f296672d2224fd09b60b8674d38ce
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-18 07:51:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var canvas = document.getElementById("screen");
+var context = canvas.getContext("2d");
+
+
+context.fillStytle="red";
+context.fillRect(20, 20, 50, 50);
+
 if(event.keyCode == 37) {
    alert('Left was pressed');
    }else if(event.keyCode == 39)
