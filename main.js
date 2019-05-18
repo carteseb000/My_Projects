e483b551f3db679a263b16e18f3d14200290ac47
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-18 08:13:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var canvas = document.getElementById("screen");
 var context = canvas.getContext("2d");
 
+var x = 20, y = 20;
+
+function move(dx, dy) {
+   x += dx;
+   y += dy;
+}
+
 function main() {
    context.fillStyle = "red";
-   context.fillRect(20, 20 , 50, 50);
+   context.fillRect(x, y, 50, 50);
+   move(3,0);
 }
 
 setInterval(main,  1000 / 60);
