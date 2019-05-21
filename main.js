d1b4816a543aad4f44e9174318aa6e4269c8bbf3
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-21 17:56:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,7 @@ var context = canvas.getContext("2d");
 var ex = 100, ey = 20;
 
 function init(){
-   var Player = function(x,y,size,speed) {
-  this.x = x;
-  this.y = y;
-  this.size = size; 
-  this.speed = speed;
-   this.update = function() {
-      ellipse(this.x,this.y,this.size,this.size);
-   };
-};
-
-var player = new Player(100,100,9,3);
-
-draw = function() {
-   player.update();
-};
-  
+   context.fillStyle = "OliveDrab";
 }
 
 function move(dx, dy) {
