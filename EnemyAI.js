087dbc672137a4894cf2d1fda0a6bf69e168b3c3
carteseb000 <50633893+carteseb000@users.noreply.github.com>
2019-05-22 07:51:28 -0400

    Update EnemyAI.js

--- EnemyAI.js
+++ EnemyAI.js
@@ -4,9 +4,17 @@ var context = canvas.getContext("2d");
 
 var x = 20, y = 20;
 
+function update() {
+
+  }
+
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   conext.fillRect(x, y, 20, 20);
+}
+
+function main() {
+  
 }
 
 function AI() {
